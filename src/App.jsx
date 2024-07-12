bcd381b3fe9714572e9f7535180762c871b6ca7e
sriram gudiseva <sriramgudiseva@gmail.com>
2024-07-12 04:26:53 +0000

    rjkhtk

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
 import { Component, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Form from './Form'
-import Add from './components/Add'
-import AddItem from './components/AddItem'
-import FilterData from './components/FilterData'
-import Tabel from './TabelPratice/Tabel'
 import CheckoutStepper from './components/stepper'
 
 const CHECK_OUT_STEPS=[
@@ -26,8 +19,7 @@ const CHECK_OUT_STEPS=[
   }
 ];
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     
 //  <Form/>
@@ -39,7 +31,7 @@ function App() {
      {/* <HoverCounter/> */}
      {/* <Tabel/> */}
      <h2>checkout</h2>
-     <CheckoutStepper stepsConfig={CHECK_OUT_STEPS} />
+      <CheckoutStepper stepsConfig={CHECK_OUT_STEPS} />
      
     
 </div>
